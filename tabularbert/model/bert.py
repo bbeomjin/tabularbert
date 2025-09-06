fix: Add normalization term to regularizer

tabularbert/model/bert.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import math
 from typing import Dict, List
 
 
@@ -98,7 +99,8 @@ class Classifier(nn.Module):
         for layer in self.fc:
             if layer is not None:
                 # nn.init.normal_(layer.weight)
-                nn.init.trunc_normal_(layer.weight, std=0.02, a=-0.04, b=0.04)
+                stdv = 1. / math.sqrt(layer.weight.size(1))
+                nn.init.normal_(layer.weight, std=stdv, a=-2 * stdv, b=2 * stdv)
                 if layer.bias is not None:
                     nn.init.zeros_(layer.bias)
     
@@ -163,7 +165,8 @@ class Regressor(nn.Module):
         for layer in self.fc:
             if layer is not None:
                 # nn.init.normal_(layer.weight)
-                nn.init.trunc_normal_(layer.weight, std=0.02, a=-0.04, b=0.04)
+                stdv = 1. / math.sqrt(layer.weight.size(1))
+                nn.init.trunc_normal_(layer.weight, std=stdv, a=-2 * stdv, b=2 * stdv)
                 if layer.bias is not None:
                     nn.init.zeros_(layer.bias)
     

tabularbert/model/embedding.py
@@ -35,8 +35,7 @@ class NumEmbedding(nn.Module):
         self._init_weights()
         
     def _init_weights(self):
-        # nn.init.normal_(self.bin_embedding.weight)
-        nn.init.trunc_normal_(self.bin_embedding.weight)
+        nn.init.normal_(self.bin_embedding.weight)
         
     def forward(self, bin_ids: torch.Tensor) -> torch.Tensor:
         """
@@ -94,8 +93,7 @@ class CatEmbedding(nn.Module):
         self._init_weights()
         
     def _init_weights(self):
-        # nn.init.normal_(self.bin_embedding.weight)
-        nn.init.trunc_normal_(self.bin_embedding.weight)
+        nn.init.normal_(self.bin_embedding.weight)
         
     def forward(self, bin_ids: torch.Tensor) -> torch.Tensor:
         """
@@ -141,8 +139,7 @@ class PositionalEmbedding(nn.Module):
         self._init_weights()
     
     def _init_weights(self):
-        # nn.init.normal_(self.embedding.weight)
-        nn.init.trunc_normal_(self.embedding.weight)
+        nn.init.normal_(self.embedding.weight)
             
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """

tabularbert/model/mlp.py
@@ -6,6 +6,7 @@ and regression on tabular data.
 
 import torch
 import torch.nn as nn
+import math
 from typing import List
 
 class MLP(nn.Module):
@@ -67,7 +68,8 @@ class MLP(nn.Module):
         for module in self.modules():
             if isinstance(module, nn.Linear):
                 # nn.init.normal_(module.weight)
-                nn.init.trunc_normal_(module.weight, std=0.02, a=-0.04, b=0.04)
+                stdv = 1. / math.sqrt(module.weight.size(1))
+                nn.init.trunc_normal_(module.weight, std=stdv, a=-2 * stdv, b=2 * stdv)
                 if module.bias is not None:
                     nn.init.zeros_(module.bias)
     